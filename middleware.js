2cd5c3ccdf967e2034e622c1c5ebc2b2f21f034e
Karan757 <karanbodkhe757@gmail.com>
2025-08-11 08:53:19 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware()
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
@@ -9,4 +8,5 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
+
 }
